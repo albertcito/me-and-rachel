ffbbed717d3e0750713799b294ed61f9cf5b1708
Albert Barrientos <albert.bar@gmail.com>
2019-04-18 17:19:04 -0400

    Updates

---
+++ precache-manifest.e4b6fef12e33bf42d7d6d9a2abda12c8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "4bbb1fa69fa070d9b1c4df2015853bbb",
+    "url": "/static/media/photo5.4bbb1fa6.jpg"
+  },
+  {
+    "revision": "167689d3d1e2502017e3e4bb2dfd0a80",
+    "url": "/static/media/photo4.167689d3.jpg"
+  },
+  {
+    "revision": "4a2e0d9f754433a115a9f4e2cd51d959",
+    "url": "/static/media/photo1.4a2e0d9f.png"
+  },
+  {
+    "revision": "74fd37f23ce38629bc47c2ad12237399",
+    "url": "/static/media/bg.74fd37f2.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a44abb28ee686a7de300",
+    "url": "/static/js/main.bfc4a766.chunk.js"
+  },
+  {
+    "revision": "2f7ea188525cc59b8dba",
+    "url": "/static/js/2.207452f7.chunk.js"
+  },
+  {
+    "revision": "a44abb28ee686a7de300",
+    "url": "/static/css/main.7d3d0e64.chunk.css"
+  },
+  {
+    "revision": "2f7ea188525cc59b8dba",
+    "url": "/static/css/2.e609e4d1.chunk.css"
+  },
+  {
+    "revision": "2d61b0b3a159f4ac18f9cd38e7c6f4aa",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/me-and-rachel/precache-manifest.5e76c4678120b508f579f25e33ed10df.js"
+  "/precache-manifest.e4b6fef12e33bf42d7d6d9a2abda12c8.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/me-and-rachel/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
