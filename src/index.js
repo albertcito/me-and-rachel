fc26efb565e73c7c5329c591d9b196db077b4af9
Albert Barrientos <albert.bar@gmail.com>
2019-04-20 11:20:34 -0400

    sadsa

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import Template from './template';
+import * as serviceWorker from 'serviceWorker';
 
 ReactDOM.render(<Template />, document.getElementById('root'));
+serviceWorker.unregister();
