f8b68419d7359afb7b2eedf99acbf1376c945b91
Albert Barrientos <albert.bar@gmail.com>
2019-08-03 22:09:11 -0400

    Updates

---
+++ precache-manifest.c04876ff1fb170d52f91a02441d79b4a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6968cc37e957157f7fda1d1841ac979",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc8e837ff92ff4636d01",
+    "url": "/static/css/main.fb71d57c.chunk.css"
+  },
+  {
+    "revision": "c22e15db35ed65f3ab2b",
+    "url": "/static/js/2.daa0c4f0.chunk.js"
+  },
+  {
+    "revision": "cc8e837ff92ff4636d01",
+    "url": "/static/js/main.4bb6cf28.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "74fd37f23ce38629bc47c2ad12237399",
+    "url": "/static/media/bg.74fd37f2.jpg"
+  },
+  {
+    "revision": "36f8ae576fba6e2fdfe73b6ce4eb1921",
+    "url": "/static/media/photo1.36f8ae57.jpg"
+  },
+  {
+    "revision": "db98c6a3a8d6ab0aee3feabaa080e517",
+    "url": "/static/media/photo4.db98c6a3.jpg"
+  },
+  {
+    "revision": "541724e30208e8fb61e205ba692ee4b9",
+    "url": "/static/media/photo5.541724e3.jpg"
+  },
+  {
+    "revision": "3aac0a9fc310d65a3a1d6240b55e8913",
+    "url": "/static/media/valdivia.3aac0a9f.jpg"
+  }
+]);
